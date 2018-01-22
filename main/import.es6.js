22ec12a6eb93cd31ea7b55c1da23d5e6697cb1b8
Mark Dittmer <markdittmer@google.com>
2018-01-22 16:18:38 -0500

    Working import script: Anomalies printed to console; have not yet filtered out constructors (which are definitely false positives)

--- main/import.es6.js
+++ main/import.es6.js
@@ -7,6 +7,8 @@ const path = require('path');
 const process = require('process');
 const url = require('url');
 
+const compat = require('mdn-browser-compat-data');
+
 require('foam2');
 
 // TODO(markdittmer): Better expose Confluence modules.
@@ -27,24 +29,85 @@ const logger = foam.log.ConsoleLogger.create();
 
 const USAGE = `USAGE:
 
-    node /path/to/import.es6.js ConfluenceDataURL
+    node /path/to/import.es6.js ConfluenceReleaseURL ConfluenceDataURL
 
+        ConfluenceReleaseURL = absolute https: or file: URL to JSON for
+                               Confluence release metadata for GridRows.
         ConfluenceDataURL = absolute https: or file: URL to JSON for Confluence
                             GridRows.`;
-if (process.argv.length !== 3) {
+if (process.argv.length !== 4) {
   console.error(USAGE);
   process.exit(1);
 }
 
-const dataUrl = url.parse(process.argv[2]);
+const releaseUrl = url.parse(process.argv[2]);
+if (releaseUrl.protocol !== 'file:' && releaseUrl.protocol !== 'https:') {
+  console.error('ConfluenceReleaseURL parameter must be file: or https: URL');
+}
+const dataUrl = url.parse(process.argv[3]);
 if (dataUrl.protocol !== 'file:' && dataUrl.protocol !== 'https:') {
   console.error('ConfluenceDataURL parameter must be file: or https: URL');
 }
 
-let inputDAO = dataUrl.protocol === 'file:' ?
-    chr.SerializableLocalJsonDAO.create({of: mdn.GridRow, path: dataUrl.pathname}) :
-    chr.SerializableHttpJsonDAO.create({of: mdn.GridRow, url: url.format(dataUrl)});
+const releaseDAO = releaseUrl.protocol === 'file:' ?
+      chr.SerializableLocalJsonDAO.create({of: chr.Release, path: releaseUrl.pathname}) :
+      chr.SerializableHttpJsonDAO.create({of: chr.Release, url: url.format(releaseUrl)});
 
-inputDAO.select().then(arraySink => {
-  logger.info(`${arraySink.array.length} API rows from Confluence`);
-});
+(async function() {
+  const releaseSink = await releaseDAO.select();
+  const releases = releaseSink.array;
+
+
+  let browserNameMap = {};
+  for (const release of releases) {
+    browserNameMap[release.browserName.toLowerCase()] = release.browserName;
+  }
+
+  const gridDAO = chr.GridDAO.create({
+    cols: releases,
+    of: mdn.GridRow,
+    delegate: dataUrl.protocol === 'file:' ?
+        chr.SerializableLocalJsonDAO.create({of: mdn.GridRow, path: dataUrl.pathname}) :
+        chr.SerializableHttpJsonDAO.create({of: mdn.GridRow, url: url.format(dataUrl)}),
+  });
+  const ctx = foam.createSubContext({gridDAO});
+
+  const rowSink = await gridDAO.select();
+  const confluenceApis = rowSink.array;
+  logger.info(`${confluenceApis.length} API rows from Confluence`);
+
+  let confluenceApiMap = {};
+  for (const api of confluenceApis) {
+    confluenceApiMap[api.id] = api;
+  }
+  for (const iface of Object.keys(compat.api)) {
+    if (iface.indexOf('__') !== -1) continue;
+    for (const api of Object.keys(compat.api[iface])) {
+      if (api.indexOf('__') !== -1) continue;
+      const id = `${iface}#${api}`;
+      let row = confluenceApiMap[id] ?
+          confluenceApiMap[id].clone(ctx) :
+          mdn.GridRow.create({id}, ctx);
+      const compatMap = compat.api[iface][api].__compat.support;
+      const compatKeys = Object.keys(compatMap)
+          .filter(key => !!browserNameMap[key]);
+      let versionInfos = [];
+      for (const key of compatKeys) {
+        if (!compatMap[key].version_added && !compatMap[key].version_removed) {
+          logger.warn(`No version info for ${key}`);
+          continue;
+        }
+
+        const versionInfo = mdn.VersionInfo.create({
+          browserName: browserNameMap[key],
+          versionAdded: compatMap[key].version_added || '',
+          versionRemoved: compatMap[key].version_removed || '',
+        }, ctx);
+        versionInfo.getConfluenceAnomalies(row);
+        versionInfos.push(versionInfo);
+      }
+      row.compatVersionInfo = versionInfos;
+      gridDAO.put(row);
+    }
+  }
+})();
