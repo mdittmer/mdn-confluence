012c026b6595875358013a7c34448935d18ddebc
Mark Dittmer <markdittmer@google.com>
2018-02-13 12:21:28 -0500

    Add Firestore NodeJS dependency and setup credentials in npm postinstall script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 
-set -e
-
-# TODO(markdittmer): This is pretty silly, but confluence require()s include
-# relative node_modules paths, so it's necessary for now.
-if [ ! -d "${1}" ]; then
-  >&2 echo "ERROR: Expected param 1 to be /local/path/to/foam2"
-  exit 1
-fi
-if [ ! -d "${2}" ]; then
-  >&2 echo "ERROR: Expected param 2 to be /local/path/to/confluence"
-  exit 1
-fi
-
 WD=$(readlink -f $(dirname "$BASH_SOURCE"))
-FD=$(readlink -f "${1}")
-CD=$(readlink -f "${2}")
 
 cd "${WD}/.."
-npm install
-cd node_modules
-rm -rf foam2
-ln -s "${FD}" foam2
-rm -rf web-api-confluence-dashboard
-ln -s "${CD}" web-api-confluence-dashboard
 
-mkdir -p "${WD}/../data/confluence"
-mkdir -p "${WD}/../data/mdn"
-mkdir -p "${WD}/../data/issues"
+if [ $(which gcloud) != "" ]; then
+  gcloud config set project "mdn-confluence"
+  mkdir -p ./.local
+  if [ -f ./.local/credentials.json ]; then
+    echo "gcloud credentials found; skipping key generation"
+  else
+    gcloud iam service-accounts keys create \
+      ./.local/credentials.json \
+      --iam-account "mdn-confluence@appspot.gserviceaccount.com"
+    if [ "${?}" != "0" ]; then
+      echo "gcloud service account key creation failed; logging in"
+      gcloud auth login
+      if [ "${?}" != "0" ]; then
+        echo "gcloud login failed"
+      fi
+      gcloud iam service-accounts keys create \
+        ./.local/credentials.json \
+        --iam-account "service-82759018549@firebase-rules.iam.gserviceaccount.com"
+      if [ "${?}" != "0" ]; then
+        echo "gcloud service account key creation failed"
+      fi
+    fi
+  fi
+else
+  echo "gcloud not installed; skipping key generation"
+fi
