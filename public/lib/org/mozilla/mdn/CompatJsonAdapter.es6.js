d45c8ab072338c8a5099eb07707e54c90b76d4c0
Mark Dittmer <markdittmer@google.com>
2018-03-05 16:05:28 -0500

    CompatJsonAdapter: Handle arrays according to the way MDN compat data presents "browser_name": [{some version info}, {more qualified version info}, ...]

--- public/lib/org/mozilla/mdn/CompatJsonAdapter.es6.js
+++ public/lib/org/mozilla/mdn/CompatJsonAdapter.es6.js
@@ -11,6 +11,19 @@ foam.CLASS({
 
   requires: ['org.mozilla.mdn.GridProperty'],
 
+  properties: [
+    {
+      class: 'Function',
+      name: 'defaultArrayMatcher',
+      value: function(array) {
+        for (let i = 0; i < array.length; i++) {
+          if (!array[i].notes) return i;
+        }
+        return -1;
+      },
+    },
+  ],
+
   methods: [
     function confluenceRowToCompatJsonFragment(row) {
       const props = row.cls_.getAxiomsByClass(this.GridProperty)
@@ -23,25 +36,27 @@ foam.CLASS({
       let support = {};
 
       for (const browserName of browserNames) {
-        var browserData = props
-            .filter(p => p.release.browserName === browserName)
-            .map(prop => { return {prop, value: prop.f(row)}; });
-        if (browserData.filter(d => !!d.value).length === 0) continue;
+        const browserProps = props
+            .filter(p => p.release.browserName === browserName);
+        if (browserProps.filter(p => !!p.f(row)).length === 0) continue;
 
         let versionAdded = null;
         let versionRemoved = null;
-        for (const {prop, value} of browserData) {
+        for (let i = 0; i < browserProps.length; i++) {
+          const browserProp = browserProps[i];
+          const value = browserProp.f(row);
           if (value && !versionAdded) {
-            const version = prop.release.browserVersion;
-            const lastDotIdx = version.lastIndexOf('.');
-            versionAdded = lastDotIdx === -1 ? version :
-                version.substr(0, lastDotIdx);
+            if (i === 0) {
+              versionAdded = true;
+            } else {
+              versionAdded = this.getVersionString(
+                  row, browserProp, browserProps);
+            }
           }
           if (versionAdded && !value) {
-            const version = prop.release.browserVersion;
-            const lastDotIdx = version.lastIndexOf('.');
-            versionRemoved = lastDotIdx === -1 ? version :
-                version.substr(0, lastDotIdx);
+            versionRemoved = this.getVersionString(
+                row, browserProp, browserProps);
+            break;
           }
         }
 
@@ -49,42 +64,108 @@ foam.CLASS({
 
         const mdnBrowserName = browserName.toLowerCase();
         support[mdnBrowserName] = {};
-        if (versionAdded) support[mdnBrowserName].version_added = versionAdded;
+        if (versionAdded !== true)
+          support[mdnBrowserName].version_added = versionAdded;
         if (versionRemoved)
           support[mdnBrowserName].version_added = versionRemoved;
       }
 
       return support;
     },
-    function patchCompatJsonFileFromConfluenceRow(json, row) {
+    function patchCompatJsonFileFromConfluenceRow(json, row, opt_arrayMatcher) {
       const patch = this.confluenceRowToCompatJsonFragment(row);
       let data = json;
-      // while (Object.keys(data).length === 1) {
-      //   for (const key of Object.keys(data)) {
-      //     data = data[key];
-      //     break;
-      //   }
-      // }
 
       data = data[row.interfaceName] || (data[row.interfaceName] = {});
       data = data[row.apiName] || (data[row.apiName] = {});
       data = data.__compat || (data.__compat = {});
       data = data.support || (data.support = {});
 
-      this.patch(data, patch);
+      this.patch(data, patch, opt_arrayMatcher);
 
       return json;
     },
-    function patch(base, patch) {
+    function patch(base, patch, opt_arrayMatcher) {
       for (const key of Object.keys(patch)) {
-        var value = patch[key];
-        if (this.isObject_(value)) {
+        const value = patch[key];
+        if (Array.isArray(base[key])) {
+          this.patchOntoArray(base[key], value, opt_arrayMatcher);
+        } else if (Array.isArray(value)) {
+          this.patchArrayOnto(base, value, key, opt_arrayMatcher);
+        } else if (this.isObject_(value)) {
           if (!base[key]) base[key] = {};
-          this.patch(base[key], patch[key]);
+          this.patch(base[key], value);
         } else {
-          base[key] = patch[key];
+          base[key] = value;
         }
       }
+    },
+    function patchOntoArray(base, patch, opt_arrayMatcher) {
+      if (Array.isArray(patch)) {
+        for (let i = 0; i < patch.length; i++) {
+          if (foam.Undefined.isInstance(patch[i])) continue;
+          if (this.isObject_(base[i]) && this.isObject_(patch[i])) {
+            this.patch(base[i], patch[i]);
+          } else {
+            base[i] = patch[i];
+          }
+        }
+      } else {
+        const idx = (opt_arrayMatcher || this.defaultArrayMatcher)(base);
+        if (idx === -1) base.unshift(patch);
+        else this.patch(base[idx], patch);
+      }
+    },
+    function patchArrayOnto(base, patch, key, opt_arrayMatcher) {
+      if (foam.Undefined.isInstance(base[key])) {
+        base[key] = [];
+        this.patchOntoArray(base[key], patch);
+        return;
+      }
+
+      const idx = (opt_arrayMatcher || this.defaultArrayMatcher)(patch);
+      if (idx === -1) {
+        patch.unshift(base[key]);
+      } else {
+        this.patch(base[key], patch[idx]);
+      }
+      base[key] = [];
+      this.patchOntoArray(base[key], patch);
+    },
+    function getVersionString(row, browserProp, browserProps) {
+      let version = browserProp.release.browserVersion;
+      const versionParts = version.split('.');
+
+      // Attempt to shrink version string to be as short as possible.
+      while (versionParts.length > 1) {
+        // Get all browsers with matching version prefix.
+        browserProps = browserProps
+            .filter(p => p.release.browserVersion.indexOf(version) !== -1);
+
+        // If fewer than 2 browser have this prefix, then shrink version number
+        // and continue.
+        if (browserProps.length < 2) {
+          versionParts.pop();
+          version = versionParts.join('.');
+          continue;
+        }
+
+        // Check whether all browsers with prefix have same value.
+        let value = browserProps[0].f(row);
+        let i;
+        for (i = 1; i < browserProps.length; i++) {
+          if (browserProps[i].f(row) !== value) break;
+        }
+        // If exited loop early, then not all browser with prefix have same
+        // value. Stop shrinking version number immediately.
+        if (i < browserProps.length) break;
+
+        // Shrink version number.
+        versionParts.pop();
+        version = versionParts.join('.');
+      }
+
+      return version;
     },
     function isObject_(value) {
       return value !== null && foam.Object.isInstance(value);
