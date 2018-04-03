fa52e54ead6b465e2e43761583f32f4b0eb99b02
Mark Dittmer <mark.s.dittmer@gmail.com>
2018-04-03 09:39:08 -0400

    WIP: Bump version number and rework build to decouple from firebase UI bits from main build process, and facilitate building correctly when embedded as a dependency

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,15 +9,24 @@ popd > /dev/null
 
 cd "${WD}/.."
 
-node node_modules/foam2/tools/build.js web,firebase
+if [ -d "./node_modules" ]; then
+  NODE_MODULES="./node_modules"
+elif [ -d "../../node_modules" ]; then
+  NODE_MODULES="../../node_modules"
+else
+  >&2 echo "mdn-confluence build error: Failed to locate node_modules"
+  exit 1
+fi
+
+node "${NODE_MODULES}/foam2/tools/build.js" web,firebase
 
 mkdir -p public/third_party/foam2
 cp \
-  node_modules/foam2/foam-bin.js \
+  "${NODE_MODULES}/foam2/foam-bin.js" \
   public/third_party/foam2
 
 mkdir -p public/third_party/confluence
 cp \
-  node_modules/web-api-confluence-dashboard/lib/web_apis/release.es6.js \
-  node_modules/web-api-confluence-dashboard/lib/grid_dao.es6.js \
+  "${NODE_MODULES}/web-api-confluence-dashboard/lib/web_apis/release.es6.js" \
+  "${NODE_MODULES}/web-api-confluence-dashboard/lib/grid_dao.es6.js" \
   public/third_party/confluence
