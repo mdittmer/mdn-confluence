4e9b88a72c6fd8ff0e21bb30f73b6821805d1e8a
gauravmahto <gauravm.git@gmail.com>
2018-07-14 18:12:31 +0530

    Added missing comments

--- scripts/post_install.js
+++ scripts/post_install.js
@@ -8,60 +8,90 @@ const path = require('path');
 const process = require('process');
 const { spawn } = require('child_process');
 
-const scriptDir = __dirname;
-const sourceRoot = path.join(scriptDir, '..');
+//
+// Path constants.
+//
 
-const isWinPlatform = (process.platform === "win32");
+const SCRIPT_DIR = __dirname;
+const SOURCE_ROOT = path.join(SCRIPT_DIR, '..');
 
+//
+// Platform vars.
+//
+
+const isWinPlatform = ('win32' === process.platform);
+
+/**
+ * Executes any arbitrary shell commands.
+ * 
+ * @param {string} cmd The command to execute. 
+ * @param {...string} arg Optional arguments for the provided command. 
+ */
 function runCommand(cmd, ...args) {
 
   try {
 
     let spawnedChild;
     const cmdWithArgs = cmd + ' ' + args.join(' ');
+
     console.log('Executing command:', cmdWithArgs);
 
+    // Execute the provided command in a win32 env.
     if (isWinPlatform) {
 
+      // Spawn a child.
       spawnedChild = spawn('cmd.exe', ['/c', cmdWithArgs]);
 
-    } else {
+    } else {  // Execute the provided command in a env. other than win32.
 
+      // Spawn a child.
       spawnedChild = spawn(cmdWithArgs, { shell: true });
 
     }
 
     if (spawnedChild) {
 
+      // Log the stdin stream.
       spawnedChild.stdout.on('data', (data) => {
+
         console.log(data.toString());
+
       });
 
+      // Log the stderr stream.
       spawnedChild.stderr.on('data', (data) => {
+
         console.error(data.toString());
+
       });
 
+      // Log the exit status.
       spawnedChild.on('exit', (code) => {
+
         console.log(`Child exited with code ${code}`);
+
       });
 
     }
 
   } catch (err) {
 
+    // log the error.
     console.error(err);
 
   }
 
 }
 
-// Run the commands.
+// Create the 'data/browser-compat-data' dir.
 if (isWinPlatform) {
 
+  // win32.
   runCommand('mkdir', path.join('data', 'browser-compat-data'));
 
 } else {
 
+  // darwin or linux.
   runCommand('mkdir', '-p', path.join('data', 'browser-compat-data'));
 
 }
