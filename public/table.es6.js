24b135cba06a77d0ae3f2af83fcc4a47fa0a622b
Philip Jägenstedt <philip@foolip.org>
2019-02-17 01:30:20 +0100

    Remove the web UI for interactive cross-referencing
    This isn't being developed/maintained further, and had some dependencies
    on web-api-confluence-dashboard. This was found while trying to prune
    dependencies as much as possible as confluence in turn has a lot of
    dependencies. The dependency remains, but is now easier to understand.

--- public/table.es6.js
+++
@@ -1,7 +0,0 @@
-let q = window.location.search;
-if (!q) {
-  q = window.location.search = '?confluence';
-}
-let script = document.createElement('script');
-script.src = `${q.substr(1)}.es6.js`;
-document.body.appendChild(script);
